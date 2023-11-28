eb73cf63b4cdbd28b7a10e2da4b052ffd1c77b37
Mike Sandman <sandmanm@battelle.org>
2023-11-28 13:01:02 -0500

    add git config

--- .local/bin/bootstrap-dotfiles.sh
+++ .local/bin/bootstrap-dotfiles.sh
@@ -7,11 +7,16 @@ curl -sSL -o /tmp/install-tools.sh https://raw.githubusercontent.com/battellecub
 bash /tmp/install-tools.sh
 
 USER_NAME="$(git config --global user.name)"
-USER_EMAIL="$(git config --global user.email)"
-
-if [[ ! "$USER_NAME" ]]; then
+[[ "$USER_NAME" ]] || {
 	read -p "What is your full name? " USER_NAME
-fi
-echo "Hello, $USER_NAME"
+	git config --global user.name "$USER_NAME"
+}
 
+USER_EMAIL="$(git config --global user.email)"
+[[ "$USER_EMAIL" ]] || {
+	read -p "What is your battelle.org email? " USER_EMAIL
+	git config --global user.email "$USER_EMAIL"
+}
+
+echo "All done!!"
 
