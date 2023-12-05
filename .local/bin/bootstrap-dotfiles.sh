1ef33fd944f3194baec26807d14718a503dc1924
Mike Sandman <sandmanm@battelle.org>
2023-12-05 12:13:11 -0500

    exec configure script

--- .local/bin/bootstrap-dotfiles.sh
+++ .local/bin/bootstrap-dotfiles.sh
@@ -6,6 +6,7 @@ echo "Let's get you setup!"
 curl -sSL -o /tmp/install-tools.sh https://raw.githubusercontent.com/battellecube/dotfiles/quick-start/.local/bin/install-tools.sh
 bash /tmp/install-tools.sh
 
-curl -sSL -o /tmp/install-tools.sh https://raw.githubusercontent.com/battellecube/dotfiles/quick-start/.local/bin/configure-dotfiles.sh
+curl -sSL -o /tmp/configure-dotfiles.sh https://raw.githubusercontent.com/battellecube/dotfiles/quick-start/.local/bin/configure-dotfiles.sh
+bash /tmp/configure-dotfiles.sh
 
 echo "All done!!"
