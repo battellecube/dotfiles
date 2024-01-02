65d74edf9f13ed88a1446c76aee3d4442e810daa
Mike Sandman <sandmanm@battelle.org>
2024-01-02 20:59:51 +0000

    add branch switching for easier testing

--- .local/bin/bootstrap-dotfiles.sh
+++ .local/bin/bootstrap-dotfiles.sh
@@ -3,10 +3,17 @@
 
 echo "Let's get you setup!"
 
-curl -sSL -o /tmp/install-tools.sh https://raw.githubusercontent.com/battellecube/dotfiles/main/.local/bin/install-tools.sh
+# export BRANCH to test a branch other than main
+# if BRANCH is zero-length, use main
+[ -z $BRANCH ] &&  {
+	BRANCH='main'
+	echo "Using the $BRANCH branch"
+}
+
+curl -sSL -o /tmp/install-tools.sh https://raw.githubusercontent.com/battellecube/dotfiles/$BRANCH/.local/bin/install-tools.sh
 bash /tmp/install-tools.sh
 
-curl -sSL -o /tmp/configure-dotfiles.sh https://raw.githubusercontent.com/battellecube/dotfiles/main/.local/bin/configure-dotfiles.sh
+curl -sSL -o /tmp/configure-dotfiles.sh https://raw.githubusercontent.com/battellecube/dotfiles/$BRANCH/.local/bin/configure-dotfiles.sh
 bash /tmp/configure-dotfiles.sh
 
 echo "All done!!"
