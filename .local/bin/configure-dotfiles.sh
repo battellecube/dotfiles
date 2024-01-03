145a39c204c9961ef127b2966d6eb66b06100ede
Mike Sandman <sandmanm@battelle.org>
2024-01-03 21:04:19 +0000

    clean up configure script

--- .local/bin/configure-dotfiles.sh
+++ .local/bin/configure-dotfiles.sh
@@ -2,86 +2,113 @@
 
 set -e
 
-# BEGIN: Git setup
-USER_NAME=$(git config --global user.name)
-[ -z "$USER_NAME" ] && {
-	read -p "What is your full name? " USER_NAME
-	git config --global user.name "$USER_NAME"
-}
+# TODO How the username and email is determined and verified will change when
+# we move to Github EMU, as it's tied to our enterprise user directory. For now
+# we'll use `git` to bootstrap the current this info for now.
+GH_USER_NAME=$(git config --global user.name)
 
-USER_EMAIL=$(git config --global user.email)
-[ -z "$USER_EMAIL" ] && {
-	read -p "What is your battelle.org email? " USER_EMAIL
-	git config --global user.email "$USER_EMAIL"
-}
-# END: Git setup
+# Show current username and prompt for a new one
+echo "Current Github username: $GH_USER_NAME"
+read -p "Enter new Github username (or press enter to keep current): " NEW_USER_NAME
 
-# need to test for scopes
-output=$(gh auth status)
-if echo "$output" | grep -q "delete_repo" && echo "$output" | grep -q "write:gpg_key"; then
-	scopes_exist='true'
+# Update the username only if a new one is provided
+if [ -n "$NEW_USER_NAME" ]; then
+	GH_USER_NAME="$NEW_USER_NAME"
 fi
-[[ "$scopes_exist" ]] || {
+
+# Set the new or existing username globally
+git config --global user.name "$GH_USER_NAME"
+
+# Get the current global Git email
+GH_USER_EMAIL=$(git config --global user.email)
+
+# Show current email and prompt for a new one
+echo "Current Github email: $GH_USER_EMAIL"
+read -p "Enter new Github email (or press enter to keep current): " NEW_USER_EMAIL
+
+# Update the email only if a new one is provided
+if [ -n "$NEW_USER_EMAIL" ]; then
+	GH_USER_EMAIL="$NEW_USER_EMAIL"
+fi
+
+# Set the new or existing email globally
+git config --global user.email "$GH_USER_EMAIL"
+
+# Fetch the current authentication status and store it in a variable
+output=$(gh auth status)
+
+# Initialize a flag variable to track if required scopes are present
+scopes_exist=false
+
+# Check if both required scopes 'delete_repo' and 'write:gpg_key' are present in the output
+if echo "$output" | grep -q "delete_repo" && echo "$output" | grep -q "write:gpg_key"; then
+	scopes_exist=true
+fi
+
+# If the required scopes are not present, refresh authentication with the necessary scopes
+if [ "$scopes_exist" = false ]; then
 	gh auth refresh --scopes delete_repo,write:gpg_key
-}
+fi
 
-type -p terraform &>/dev/null || {
-	echo -n "Installing Terraform CLI..."
-	install-terraform-cli &>/dev/null
-	echo "done."
-}
+# Fetch GPG keys associated with a specific email domain from GitHub and store them in an array
+GPG_KEYS=($(gh gpg-key list | awk '/battelle.org/{print $2}'))
 
-# BEGIN: GPG setup
-GPG_KEYS=(`gh gpg-key list | awk '/battelle.org/{print $2}'`)
-
+# Initialize a flag to track if a key is found
 key_found=false
 
+# Iterate over the keys to check if they are present in the local GPG keyring
 for k in "${GPG_KEYS[@]}"; do
 	if gpg --list-keys "$k" &> /dev/null; then
-		SIGNING_KEY=$k
+		GH_SIGNING_KEY=$k
 		key_found=true
 		break
 	fi
 done
 
-if [ "$key_found" == "false" ]; then
+# If no key is found, create a new GPG key
+if [ "$key_found" = "false" ]; then
 	echo "Creating a new GPG key"
-	TEMP_DIR=$(mktemp -dq )
+
+	# Create a temporary directory for GPG configuration and key generation logs
+	TEMP_DIR=$(mktemp -dq)
 	CONFIG_FILE="$TEMP_DIR/key-config"
+
+	# Generate a GPG key configuration file
 	cat > "$CONFIG_FILE" <<-EOF
 		%echo Generating a basic OpenPGP key
 		Key-Type: RSA
 		Key-Length: 4096
 		Subkey-Type: RSA
 		Subkey-Length: 4096
-		Name-Real: $USER_NAME
-		Name-Email: $USER_EMAIL
+		Name-Real: $GH_USER_NAME
+		Name-Email: $GH_USER_EMAIL
 		Expire-Date: 0
 		%no-protection
 		%commit
 		%echo Done
 	EOF
-	gpg --batch --generate-key "$CONFIG_FILE" &>$TEMP_DIR/gpg.log
 
-	SIGNING_KEY=$(cat $TEMP_DIR/gpg.log | grep 'key .* marked as ultimately trusted' | awk '{print $3}')
+	# Generate a GPG key using the configuration file
+	gpg --batch --generate-key "$CONFIG_FILE" &> $TEMP_DIR/gpg.log
 
-	gpg --export --armor $SIGNING_KEY > $TEMP_DIR/pub_key.asc
+	# Extract the generated key ID from the log
+	GH_SIGNING_KEY=$(grep 'key .* marked as ultimately trusted' $TEMP_DIR/gpg.log | awk '{print $3}')
+
+	# Export the public key to a file
+	gpg --export --armor "$GH_SIGNING_KEY" > "$TEMP_DIR/pub_key.asc"
 
 	echo "Uploading new key to Github"
-	gh gpg-key add $TEMP_DIR/pub_key.asc
+	# Upload the new GPG key to GitHub
+	gh gpg-key add "$TEMP_DIR/pub_key.asc"
 fi
-# END: GPG setup
-
-#LOCAL_KEYS=(`gpg --list-keys --keyid-format long $USER_EMAIL 2>/dev/null | grep 'pub' | awk '{print $2}' | cut -d'/' -f2`)
-#MATCHING_KEYS=(`comm -12 <( for k in "${LOCAL_KEYS[@]}"; do echo $k; done | sort) <(for k in "${GPG_KEYS[@]}"; do echo $k; done |sort)`)
 
 cat <<END
 
 ---------------------------
 Git
-  username: $USER_NAME
-  email:    $USER_EMAIL
-  key:      $SIGNING_KEY
+  username: $GH_USER_NAME
+  email:    $GH_USER_EMAIL
+  key:      $GH_SIGNING_KEY
 ---------------------------
 
 END
